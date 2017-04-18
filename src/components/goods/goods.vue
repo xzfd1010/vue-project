3f38546fc726ce1f7a462482f58925a20f8f68fb
Nick <zhongcheng509@gmail.com>
2017-04-18 11:03:34 +0800

    利用betterScroll完成商品页的左右滑动联动效果

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" v-el:menu-wrapper>
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="item in goods" class="menu-item" :class="{'current':currentIndex === $index}"
+            @click="selectMenu($index,$event)">
           <span class="text border-1px-bottom">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{item.name}}
@@ -10,9 +11,9 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" v-el:foods-wrapper>
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item border-1px-bottom">
@@ -23,8 +24,7 @@
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span class="count">月售{{food.sellCount}}份</span>
-                  <span>好评率{{food.rating}}%</span>
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
                   <span class="now">¥{{food.price}}</span>
@@ -40,7 +40,10 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+
   const ERR_OK = 0
+
   export default{
     props: {
       seller: {
@@ -49,7 +52,23 @@
     },
     data() {
       return {
-        goods: []
+        goods: [],
+        listHeight: [],
+        scrollY: 0
+      }
+    },
+    computed: {
+      currentIndex() {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i]
+          let height2 = this.listHeight[i + 1]
+          // !height2是为了防止最后一个时候，!height2是undefined
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            // 返回当前的索引，对应列表项
+            return i
+          }
+        }
+        return 0
       }
     },
     created() {
@@ -59,8 +78,52 @@
         response = response.body
         if (response.errno === ERR_OK) {
           this.goods = response.data
+          // 在下次更新DOM时执行initScroll和calculateHeight，也就是DOM已经存在的时候，应该有别的生命周期函数可以放
+          this.$nextTick(() => {
+            console.log(1)
+            this._initScroll()
+            this._calculateHeight()
+          })
         }
       })
+    },
+    methods: {
+      _initScroll() {
+        this.menuScroll = new BScroll(this.$els.menuWrapper, {
+          click: true
+        })
+
+        this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
+//          希望scroll在滚动时，实时告知滚动的位置，应该是个参数吧
+          probeType: 3
+        })
+        this.foodsScroll.on('scroll', (pos) => {
+          // 获取滚动的为止，abs
+          this.scrollY = Math.abs(Math.round(pos.y))
+        })
+      },
+      _calculateHeight() {
+//        找到每个li
+        let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook')
+        let height = 0
+        this.listHeight.push(height)
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i]
+          height += item.clientHeight
+//          构造一个递增的区间数组
+          this.listHeight.push(height)
+        }
+      },
+      selectMenu(index, event) {
+        // 为了防止pc端点击事件派发两次，传递event，自己派发的click和浏览器原生的click有不同，浏览器的没有event._constructed
+        if (!event._constructed) {
+          return
+        }
+//        console.log(index)
+        let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook')
+        let el = foodList[index]
+        this.foodsScroll.scrollToElement(el, 300)
+      }
     }
   }
 </script>
@@ -85,6 +148,15 @@
         width: 56px
         padding: 0 12px
         line-height: 14px
+        &.current
+          z-index: 10
+          margin-top: -1px
+          line-height: 14px
+          background: #fff;
+          font-weight: 700
+          font-size: 12px
+          .text
+            border-none()
         .icon
           display: inline-block
           vertical-align: top
@@ -115,55 +187,50 @@
         padding-left: 14px
         height: 26px
         line-height: 26px
-        border-left:2px solid #d9dde1
+        border-left: 2px solid #d9dde1
         font-size: 12px
-        color:rgb(147,153,159)
+        color: rgb(147, 153, 159)
         background: #f3f5f7;
       .food-item
         display: flex
         margin: 18px
         padding-bottom: 18px;
-        border-1px-bottom(rgba(7,17,27,0.1))
+        border-1px-bottom(rgba(7, 17, 27, 0.1))
         &:last-child
           border-none()
-          margin-bottom:0;
+          margin-bottom: 0;
         .icon
-          flex:0 0 57px;
+          flex: 0 0 57px;
           margin-right: 10px;
         .content
-          flex:1
+          flex: 1
           .name
             margin: 2px 0 8px 0;
             height: 14px
             line-height: 14px
             font-size: 14px;
-            color:rgb(7,17,27)
-          .desc,.extra
-            line-height:10px;
+            color: rgb(7, 17, 27)
+          .desc, .extra
+            line-height: 10px;
             font-size: 10px;
-            color:rgb(147,153,159)
+            color: rgb(147, 153, 159)
           .desc
             margin-bottom: 8px;
+            line-height: 12px
           .extra
-            font-size: 0
-            span
-              font-size: 10px
-              line-height: 10px;
             .count
               margin-right: 12px;
           .price
-            font-weight:700
+            font-weight: 700
             line-height: 24px
             .now
               margin-right: 8px
-              color:rgb(240,20,20)
+              color: rgb(240, 20, 20)
               font-size: 14px
             .old
               text-decoration: line-through;
               font-size: 10px
-              color:rgb(147,153,159)
-
-
+              color: rgb(147, 153, 159)
 
 
 </style>
