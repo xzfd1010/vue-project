d71e7fce59ecf664a25129629f6f0e648c9eab48
Nick <zhongcheng509@gmail.com>
2017-04-21 20:00:51 +0800

    商品详情页

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="shopcart">
-    <div class="content">
+    <div class="content" @click="toggleList">
       <div class="content-left">
         <div class="logo-wrapper">
           <div class="logo" :class="{'highlight':totalCount>0}">
@@ -11,7 +11,7 @@
         <div class="price" :class="{'highlight':totalCount>0}">¥ {{totalPrice}}</div>
         <div class="desc">另需配送费¥{{deliveryPrice}}元</div>
       </div>
-      <div class="content-right">
+      <div class="content-right" @click.stop.prevent="pay">
         <div class="pay" :class="payClass">
           {{payDesc}}
         </div>
@@ -22,10 +22,33 @@
         <div class="inner inner-hook"></div>
       </div>
     </div>
+    <div class="shopcart-list" v-show="listShow" transition="fold">
+      <div class="list-header">
+        <h1 class="title">购物车</h1>
+        <span class="empty" @click="empty">清空</span>
+      </div>
+      <div class="list-content" v-el:list-content>
+        <ul>
+          <li class="food" v-for="food in selectFoods">
+            <span class="name">{{food.name}}</span>
+            <div class="price">
+              <span>¥{{food.price*food.count}}</span>
+            </div>
+            <div class="cartcontrol-wrapper">
+              <cartcontrol :food="food"></cartcontrol>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
+  <div class="list-mask" @click="hideList" v-show="listShow" transition="fade"></div>
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+  import cartcontrol from 'components/cartcontrol/cartcontrol'
+
   export default {
     props: {
       deliveryPrice: {
@@ -62,7 +85,8 @@
             show: false
           }
         ],
-        dropBalls: []
+        dropBalls: [],
+        fold: true
       }
     },
     computed: {
@@ -97,6 +121,27 @@
         } else {
           return 'enough'
         }
+      },
+      listShow() {
+        if (!this.totalCount) {
+          this.fold = true
+          return false
+        }
+        let show = !this.fold
+        // 只有列表展示时，才需要对列表初始化
+        // 触发DOM
+        if (show) {
+          this.$nextTick(() => {
+            if (!this.scroll) {
+              this.scroll = new BScroll(this.$els.listContent, {
+                click: true
+              })
+            } else {
+              this.scroll.refresh()
+            }
+          })
+        }
+        return show
       }
     },
     methods: {
@@ -114,6 +159,27 @@
             return
           }
         }
+      },
+      toggleList() {
+        if (!this.totalCount) {
+          return
+        }
+//      通过取反进行折叠／展开
+        this.fold = !this.fold
+      },
+      empty() {
+        this.selectFoods.forEach((food) => {
+          food.count = 0
+        })
+      },
+      hideList() {
+        this.fold = true
+      },
+      pay() {
+        if (this.totalPrice < this.minPrice) {
+          return
+        }
+        window.alert(`支付${this.totalPrice}元`)
       }
     },
     // 利用transition来实现动画，钩子函数
@@ -144,7 +210,7 @@
           }
         },
         enter(el) {
-          // 手动触发浏览器重绘
+          // 手动触发浏览器重绘 reflow
           /* eslint-disable no-unused-vars*/
           let rf = el.offsetHeight
           this.$nextTick(() => {
@@ -161,15 +227,21 @@
           let ball = this.dropBalls.shift()
           if (ball) {
             ball.show = false
+            /* eslint-disable no-unused-vars*/
+            let rf = el.offsetHeight
             el.style.display = 'none'
           }
         }
       }
+    },
+    components: {
+      cartcontrol
     }
   }
 </script>
 
 <style lang="stylus" type="text/stylus">
+  @import "../../common/stylus/mixin.styl"
   .shopcart
     position: fixed
     left: 0
@@ -268,12 +340,80 @@
         z-index: 200
         /*最终状态的位置*/
         &.drop-transition
-          transition: all .4s cubic-bezier(.49,-.29,.75,.41)
-        .inner
-          width: 16px
-          height: 16px
-          border-radius: 50%
-          background: rgb(0, 160, 220);
-          transition: all .4s linear
+          transition: all .4s cubic-bezier(.49, -.29, .75, .41)
+          .inner
+            width: 16px
+            height: 16px
+            border-radius: 50%
+            background: rgb(0, 160, 220);
+            transition: all .4s linear
 
+    .shopcart-list
+      position: absolute
+      top: 0
+      left: 0
+      z-index: -1
+      width: 100%
+      &.fold-transition
+        transition: all .5s
+        /*终态是-100%，相对于自身的便宜*/
+        transform: translate3d(0, -100%, 0)
+      &.fold-enter, &.fold-leave
+        transform: translate3d(0, 0, 0)
+      .list-header
+        height: 40px
+        line-height: 40px
+        padding: 0 18px
+        background: #f3f5f7
+        border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+        .title
+          float: left
+          font-size: 14px
+          color: rgb(7, 17, 27)
+        .empty
+          float: right
+          font-size: 12px
+          color: rgb(0, 160, 220)
+      .list-content
+        padding: 0 18px
+        max-height: 217px
+        overflow: hidden
+        background: #fff
+        .food
+          position: relative
+          padding: 12px 0
+          box-sizing: border-box
+          border-1px-bottom(rgba(7, 17, 27, .1))
+          .name
+            line-height: 24px
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .price
+            position: absolute
+            right: 90px
+            bottom: 12px
+            line-height: 24px
+            font-size: 14px
+            font-weight: 700
+            color: rgb(240, 20, 20)
+          .cartcontrol-wrapper
+            position: absolute
+            right: 0
+            bottom: 6px
+
+  .list-mask
+    position: fixed
+    top: 0
+    left: 0
+    width: 100%
+    height: 100%
+    z-index: 40
+    backdrop-filter: blur(10px)
+    transition: all .5s
+    &.fade-transition
+      opacity: 1
+      background: rgba(7, 17, 27, .6)
+    &.fade-enter, &.fade-leave
+      opacity: 0
+      background: rgba(7, 17, 27, 0);
 </style>
